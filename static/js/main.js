2ea26a62feadc1900969cdffd6a5f36275810937
doylemc <mikeodoyle@gmail.com>
2019-12-11 21:57:26 +0000

    latest updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,45 @@
+// function resize() {
+//     var heights = window.innerHeight;
+//     document.getElementById("page-content").style.height = heights - 50 + "px";
+// }
+// resize();
+// window.onresize = function () {
+//     resize();
+// };
+
 const player = new Plyr('#player', {
-    title: 'Example Title',
+    title: 'GetInspired365',
 });
 
-player.play(); // Start playback
+player.on('ready', event => {
+    // window.scroll({
+    //     top: 250,
+    //     left: 0,
+    //     behavior: 'smooth'
+    // });
+    const instance = event.detail.plyr;
+    player.play(); // Start playback
+    //player.fullscreen.enter();
+});
+
+// player.on('playing', event => {
+//     document.getElementById("site-nav").style.visibility = "hidden";
+//     document.getElementById("page-header").style.visibility = "hidden";
+//     document.getElementById("section-tags").style.visibility = "hidden";
+//     document.getElementById("page-footer").style.visibility = "hidden";
+// });
+
+// player.on('pause', event => {
+//     document.getElementById("site-nav").style.visibility = "visible";
+//     document.getElementById("page-header").style.visibility = "visible";
+//     document.getElementById("section-tags").style.visibility = "visible";
+//     document.getElementById("page-footer").style.visibility = "visible";
+// });
+
+player.on('ended', event => {
+    document.getElementById("navigate_next").click();
+    // document.getElementById("site-nav").style.visibility = "visible";
+    // document.getElementById("page-header").style.visibility = "visible";
+    // document.getElementById("section-tags").style.visibility = "visible";
+    // document.getElementById("page-footer").style.visibility = "visible";
+});
